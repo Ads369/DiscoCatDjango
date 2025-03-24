7effffaaa5d0179be0f0397b47649401b1354558
Dmitriy Aleksandrov <Adsis369@gmail.com>
2025-03-24 20:39:37 +0500

    Lets start

--- backend/evaluator/urls.py
+++ backend/evaluator/urls.py
@@ -3,16 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from users.views import UUIDLoginView, login_view, rooms_view
-from voting.views import nickname_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", login_view, name="login"),
+    path("", login_view, name="login"),
     path("rooms/", rooms_view, name="rooms"),
     path("api/auth/login/", UUIDLoginView.as_view(), name="api_login"),
-    # # Include voting.urls without the api prefix since it's already defined there
+    # Include voting.urls without the api prefix since it's already defined there
     path("", include("voting.urls")),  # This includes all of voting/urls.py at root
-    path("", nickname_view, name="login"),
 ]
 
 # Serve static and media files in development
